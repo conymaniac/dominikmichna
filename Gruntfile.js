ef247d17c1c4985f14bce9b85a8dbef592575bff
Dominik Michna <dominik.michna@cony.local>
2016-06-20 17:24:21 +0200

    v.black – simple & responsive

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+/*
+ * grunt-init
+ * https://gruntjs.com/
+ *
+ * Copyright (c) 2016 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    less: {
+        dev: {
+            files: {
+                'dist/static/css/style.min.css': 'src/less/style.less'
+            }
+        },
+        prod: {
+            options: {
+                compress: true
+            },
+            files: {
+                'dist/static/css/style.min.css': 'src/less/style.less'
+            }
+        }
+    },
+
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Whenever the "test" task is run, run some tests.
+  grunt.registerTask('dev', ['less:dev']);
+  grunt.registerTask('prod', ['less:prod']);
+
+};
---
+++ dist/static/js/script.js
@@ -0,0 +1,36 @@
+/**
+ * Just a little script to adjust width according the headline
+ */
+
+(function () {
+    var _content = null;
+    var _title = null;
+    var _topLink = null;
+    var _diff = null;
+    var _wWidth = null;
+
+    var _position = function() {
+        // elements
+    	_content = document.querySelector('.content');
+	    _title = document.querySelector('.content__title');
+	    _topLink = document.querySelector('.content__link');
+
+        // window size
+        _wWidth = Object.prototype.hasOwnProperty.call(window, 'innerWidth') ? window.innerWidth : document.body.clientWidth;
+        
+        console.info('test');
+        console.log(_wWidth);
+
+        if (_wWidth >= 896) {
+        	_diff = _content.offsetWidth - _title.offsetWidth;
+            _topLink.style.right =  _diff/16 + 'em';
+            _topLink.style.top =  -(_topLink.offsetHeight/16) + 'em';
+        } else {
+            _topLink.removeAttribute('style');
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", _position.bind(this));
+    window.addEventListener("resize", _position.bind(this));
+    document.addEventListener("resize", _position.bind(this));
+})();
